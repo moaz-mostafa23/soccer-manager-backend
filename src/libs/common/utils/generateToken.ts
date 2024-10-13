e495b2b6c111743ad2a10a7ab03ef36fccf2a364
Moaz Mostafa <m.mostafa@calo.app>
2024-10-13 16:11:42 +0300

    Remove all files

--- src/libs/common/utils/generateToken.ts
+++
@@ -1,8 +0,0 @@
-import * as jwt from 'jsonwebtoken';
-
-const generateToken = (id: string): string =>
-    jwt.sign({ user: { id } }, process.env.JWT_SECRET!, {
-        expiresIn: '60d',
-    });
-
-export default generateToken;
