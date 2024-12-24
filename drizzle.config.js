87eb1d9ecf748291f0ffab80635db9dc0c501ac2
Moaz Mostafa <m.mostafa@calo.app>
2024-12-24 22:10:29 +0300

    more stuffs

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     dialect: 'postgresql',
     dbCredentials: {
         url: process.env.DATABASE_URL || '',
+        ssl: true,
     },
     verbose: true,
     strict: true,
