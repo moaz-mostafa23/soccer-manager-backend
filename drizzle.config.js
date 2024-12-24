419cdeed0fd0effa70b96a0f472e4d98cf2c2df6
Moaz Mostafa <m.mostafa@calo.app>
2024-12-24 22:18:58 +0300

    Enable SSL in drizzle config

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'drizzle-kit';
+
 export default defineConfig({
-    schema: './src/user-service/db/schema.ts',
-    out: './drizzle',
-    dialect: 'postgresql',
-    dbCredentials: {
-        url: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false,
-        },
+  schema: './src/libs/db/schema.ts',
+  out: './drizzle',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
     },
-    verbose: true,
-    strict: true,
+  },
+  verbose: true,
+  strict: true,
 });
