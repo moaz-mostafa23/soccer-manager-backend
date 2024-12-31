2b63e837066a6a54ffdf0057c4e9ebe15dbd07e5
Moaz Mostafa <m.mostafa@calo.app>
2024-12-31 10:10:17 +0300

    remove extra logs

--- src/libs/common/middleware/auth.ts
+++ src/libs/common/middleware/auth.ts
@@ -6,24 +6,15 @@ interface AuthenticatedRequest extends Request {
 }
 
 export const authMiddleware = (req: AuthenticatedRequest, res: Response, next: NextFunction): void => {
-    console.log("🚀 ~ authMiddleware ~ req:", req)
-    // const token = req.header('Authorization');
     const token = req.header('Authorization')?.split(' ')[1];
-    console.log("🚀 ~ authMiddleware ~ token (raw):", token);
-
-    console.log("🚀 ~ authMiddleware ~ token:", token)
-
     if (!token) {
         res.status(401).json({ message: 'No token, no bueno senor' });
         return;
     }
 
     try {
-        console.log("🚀 ~ authMiddleware ~ process.env.JWT_SECRET!:", process.env.JWT_SECRET!)
         const decoded = jwt.verify(token, process.env.JWT_SECRET!);
-        console.log("🚀 ~ authMiddleware ~ decoded:", decoded)
         req.user = decoded;
-        console.log("🚀 ~ authMiddleware ~ req.user:", req.user)
         next();
     } catch (err) {
         res.status(401).json({ message: 'token is shit, try again' });
